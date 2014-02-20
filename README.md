4bdd50eff9e6df8cf8b13bff7ddf8ed11bb6bad7
Gourav Tiwari <gouravtiwari21@gmail.com>
2014-02-21 02:37:30 +0530

    First working version of contineo, need to add specs

--- README.md
+++ README.md
@@ -2,3 +2,27 @@ contineo
 ========
 
 connect multiple databases to your rails application with ease!
+
+=== sample database.yml:
+
+    development:
+      adapter: sqlite3
+      database: db/development.sqlite3
+      pool: 5
+      timeout: 5000
+
+    other_development:
+      adapter: sqlite3
+      database: db/other.sqlite3
+      pool: 5
+      timeout: 5000
+
+=== Access with ease
+
+Create your model as:
+
+    class Part < Other
+      contineo
+    end
+
+
--- lib/contineo.rb
+++ lib/contineo.rb
@@ -2,4 +2,18 @@ require "contineo/version"
 require 'active_record'
 
 module Contineo
+  CONFIG = YAML.load_file("config/database.yml")
+  CONFIG.each do |db_env, connection_hash|
+    env = db_env.split('_').last
+    
+    if db_env.split('_').size > 1
+      db  = db_env.split('_'+env).first
+      
+      klass = Object.const_set(db.camelcase, Class.new(ActiveRecord::Base) { 
+        self.abstract_class = true
+      })
+
+      klass.define_singleton_method(:contineo) { establish_connection db_env }
+    end
+  end
 end
