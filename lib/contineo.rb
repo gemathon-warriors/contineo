bd2e1bdfc7f56dc0a1cd3c7ecea317b1dd7d99cc
Gourav Tiwari <gouravtiwari21@gmail.com>
2014-02-21 08:31:09 +0530

    Pure ruby meta-programming :sunglasses:

--- lib/contineo.rb
+++ lib/contineo.rb
@@ -3,14 +3,18 @@ require 'active_record'
 
 module Contineo
   CONFIG = YAML.load_file("config/database.yml")
+  
   CONFIG.each do |db_env, connection_hash|
     env = db_env.split('_').last
     
-    if db_env.split('_').size > 1
+    if db_env.split('_').size > 1 && Rails.env == env
       db  = db_env.split('_'+env).first
       
       klass = Object.const_set(db.camelcase, Class.new(ActiveRecord::Base) { 
         self.abstract_class = true
+        def self.inherited(base)
+          contineo
+        end
       })
 
       klass.define_singleton_method(:contineo) { establish_connection db_env }
