407ceb5917db19097bfe0f5cfa6ce4e173acaa9e
Gourav Tiwari <gouravtiwari21@gmail.com>
2014-02-24 18:26:03 +0530

    Refactored and created complete test suite with TDD :relaxed:

--- lib/contineo.rb
+++ lib/contineo.rb
@@ -3,27 +3,45 @@ require 'active_record'
 require 'rails'
 
 module Contineo
-  CONFIG = begin
-    YAML.load_file(CONFIG_PATH)
-  rescue NameError => e
-    YAML.load_file("config/database.yml")  
+  def self.db(db_env)
+    db_env.split('_'+ env(db_env)).first.camelcase
   end
 
-  CONFIG.each do |db_env, connection_hash|
-    env = db_env.split('_').last
+  def self.env(db_env)
+    db_env.split('_').last
+  end
 
-    if db_env.split('_').size > 1 && ::Rails.env == env
-      db  = db_env.split('_'+env).first
+  def self.other_than_application_db?(db_env)
+    db_env.split('_').size > 1
+  end
+
+  def self.define_db_connection_class(db_env)
+    klass = Object.const_set(db(db_env), Class.new(ActiveRecord::Base) { 
+              self.abstract_class = true
+              def self.inherited(base)
+                contineo
+              end
+            })
       
-      klass = Object.const_set(db.camelcase, Class.new(ActiveRecord::Base) { 
-        self.abstract_class = true
-        def self.inherited(base)
-          contineo
-        end
-      })
-      
-      klass.define_singleton_method(:contineo) { establish_connection db_env }
+    klass.define_singleton_method(:contineo) { establish_connection db_env }
+    klass
+  end
+
+  def self.configurations
+    begin
+      YAML.load_file(CONFIG_PATH)
+    rescue NameError => e
+      YAML.load_file("config/database.yml")  
     end
   end
 
+  def self.create_connections
+    configurations.each do |db_env, connection_hash|
+      if other_than_application_db?(db_env) && ::Rails.env == env(db_env)
+        define_db_connection_class(db_env)
+      end
+    end
+  end
+
+  create_connections
 end
--- spec/contineo_spec.rb
+++ spec/contineo_spec.rb
@@ -1,11 +1,42 @@
 require 'spec_helper'
 
 describe 'Contineo multiple databases' do
-  it "should return count for first database as zero" do
-    Doctor.count.should be_zero
+  context "test connection and run basic AREL query" do
+    it "should return count for first database as zero" do
+      Doctor.count.should be_zero
+    end
+
+    it "should return count for second database as zero" do
+      Teacher.count.should be_zero
+    end
   end
 
-  it "should return count for second database as zero" do
-    Teacher.count.should be_zero
+  context '.env' do
+    it 'should return environment name from configuration name' do
+      Contineo.env('other_db_qa').should == 'qa'
+    end
+  end
+
+  context '.db' do
+    it 'should return db class from configuration name' do
+      Contineo.db('other_db_qa').should == 'OtherDb'
+    end
+  end
+
+  context '.other_than_application_db?' do
+    it 'should return true when db in question is other than application db' do
+      Contineo.other_than_application_db?('other_db_qa').should be_true
+    end
+
+    it 'should return false when db in question is application db' do
+      Contineo.other_than_application_db?('qa').should be_false
+    end
+  end
+
+  context '.define_db_connection_class' do
+    it 'should define connection class for First DB with contineo method' do
+      Contineo.define_db_connection_class('other_db_qa').should == OtherDb
+      OtherDb.respond_to?('contineo').should be_true
+    end
   end
 end
